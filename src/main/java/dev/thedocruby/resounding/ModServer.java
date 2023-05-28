6cfbdc6dd8be06ecfca9242e9aa5206a180e4ca1
Jack Gabriel C. Acodili <112671361+acodili-jg@users.noreply.github.com>
2023-05-29 07:55:26 +0800

    Document ModServer.java
     - based of on DedicatedServerModInitializer's documentation

--- src/main/java/dev/thedocruby/resounding/ModServer.java
+++ src/main/java/dev/thedocruby/resounding/ModServer.java
@@ -4,7 +4,13 @@ import dev.thedocruby.resounding.config.BlueTapePack.ConfigManager;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.EnvType;
 
+/**
+ * Resounding's initializer, ran only on {@link EnvType#SERVER}.
+ */
 public class ModServer implements DedicatedServerModInitializer {
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void onInitializeServer() {
 		Engine.env = EnvType.SERVER;
