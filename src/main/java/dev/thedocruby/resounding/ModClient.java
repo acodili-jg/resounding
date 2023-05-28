0a4271c3a89bb07b960d3da1709e83b956ffdf3a
Jack Gabriel C. Acodili <112671361+acodili-jg@users.noreply.github.com>
2023-05-29 07:49:03 +0800

    Document ModClient.java
     - based of on ClientModInitializer's documentation

--- src/main/java/dev/thedocruby/resounding/ModClient.java
+++ src/main/java/dev/thedocruby/resounding/ModClient.java
@@ -4,7 +4,13 @@ import dev.thedocruby.resounding.config.BlueTapePack.ConfigManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 
+/**
+ * Resounding's initializer, ran only on {@link EnvType#CLIENT}.
+ */
 public class ModClient implements ClientModInitializer {
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void onInitializeClient() {
 		Engine.env = EnvType.CLIENT;
