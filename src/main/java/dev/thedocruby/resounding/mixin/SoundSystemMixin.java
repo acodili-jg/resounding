5d550739f2ed37a82f61967a7cc593133e239abf
Jack Gabriel Acodili <112671361+acodili-jg@users.noreply.github.com>
2024-03-03 15:17:41 +0800

    Update SoundSystemMixin.java
    Reduce minimum monitor size requirements, horizontal scrolling, and future diff size. Renamed some parameters.

--- src/main/java/dev/thedocruby/resounding/mixin/SoundSystemMixin.java
+++ src/main/java/dev/thedocruby/resounding/mixin/SoundSystemMixin.java
@@ -5,7 +5,7 @@ import dev.thedocruby.resounding.config.PrecomputedConfig;
 import dev.thedocruby.resounding.toolbox.SourceAccessor;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.sound.*;
+import net.minecraft.client.sound.*; // FIXME: must expand
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.Identifier;
 import org.objectweb.asm.Opcodes;
@@ -21,40 +21,85 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import java.util.Iterator;
 import java.util.Map;
 
-import static dev.thedocruby.resounding.Engine.mc;
-import static dev.thedocruby.resounding.config.PrecomputedConfig.pC;
-
 @Environment(EnvType.CLIENT)
 @Mixin(SoundSystem.class)
 public class SoundSystemMixin {
+    @Final
+    @Shadow
+    private SoundListener listener;
 
-	@Shadow @Final private SoundListener listener;
-
-	@Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At(value = "FIELD", target = "net/minecraft/client/sound/SoundSystem.sounds : Lcom/google/common/collect/Multimap;"), locals = LocalCapture.CAPTURE_FAILHARD)
-	private void soundInfoYeeter(SoundInstance sound, CallbackInfo ci, WeightedSoundSet weightedSoundSet, Identifier identifier, Sound sound2, float f, float g, SoundCategory soundCategory){
-		if (!Engine.on) return;
-		Engine.recordLastSound(sound, this.listener); // TODO: do this better maybe
-	}
+    @Inject(
+    	method = "play(Lnet/minecraft/client/sound/SoundInstance;)V",
+        at = @At(
+            value = "FIELD",
+            target = "net/minecraft/client/sound/SoundSystem.sounds : Lcom/google/common/collect/Multimap;"
+        ),
+        locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    private void soundInfoYeeter(
+        final SoundInstance instance,
+        final CallbackInfo callback,
+        final WeightedSoundSet weightedSoundSet,
+        final Identifier identifier,
+        final Sound sound,
+        final float f,
+        final float g,
+        final SoundCategory category
+    ) {
+        if (Engine.on) {
+            // TODO: do this better maybe
+            Engine.recordLastSound(instance, this.listener);
+        }
+    }
 
 //  @Inject(method = "tick()V", at = @At(value = "HEAD"))
-//  private void ticker(CallbackInfo ci){ Air.updateSmoothedRain(); }
+//  private void ticker(final CallbackInfo callback) {
+//      Air.updateSmoothedRain();
+//  }
 
-	@ModifyArg(method = "getAdjustedVolume", at = @At(value = "INVOKE", target = "net/minecraft/util/math/MathHelper.clamp (FFF)F"), index = 0)
-	private float volumeMultiplierInjector(float vol){ if (!Engine.on) return vol; return vol * PrecomputedConfig.globalVolumeMultiplier; }
+    @ModifyArg(
+        method = "getAdjustedVolume",
+        at = @At(
+            value = "INVOKE",
+            target = "net/minecraft/util/math/MathHelper.clamp (FFF)F"
+        ),
+        index = 0
+    )
+    private float volumeMultiplierInjector(final float vol) {
+        if (Engine.on) {
+            return vol * PrecomputedConfig.globalVolumeMultiplier;
+        } else {
+            return vol;
+        }
+    }
 
-	@SuppressWarnings("InvalidInjectorMethodSignature")
-	@Inject(method = "tick()V", at = @At(value = "JUMP", opcode = Opcodes.IFEQ, ordinal = 3), locals = LocalCapture.CAPTURE_FAILHARD)
-	private void recalculate(CallbackInfo ci, Iterator<?> iterator, Map.Entry<?, ?> entry, Channel.SourceManager f, SoundInstance g, float vec3d){
-		if (!Engine.on) return;
-		if (mc.world != null && mc.world.getTime()%pC.srcRefrRate ==0){
-			f.run((s) -> ((SourceAccessor)s).calculateReverb(g, this.listener));
-			/*mc.world.getRegistryManager().get(Registry.BLOCK_KEY).streamTags().forEachOrdered(
-					(tagKey) -> {
-						Engine.LOGGER.info(tagKey.registry().getValue());
-					}
-			);*/
-		}
-			//((SourceAccessor)null)
-	}
-
+    @SuppressWarnings("InvalidInjectorMethodSignature")
+    @Inject(
+        method = "tick()V",
+        at = @At(
+            value = "JUMP",
+            opcode = Opcodes.IFEQ,
+            ordinal = 3
+        ),
+        locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    private void recalculate(
+        final CallbackInfo callback,
+        final Iterator<?> iterator,
+        final Map.Entry<?, ?> entry,
+        final Channel.SourceManager f,
+        final SoundInstance g,
+        final float vec3d
+    ) {
+	if (!Engine.on) { return; }
+	final var world = Engine.mc.world;
+        if (world != null && world.getTime() % .srcRefrRate == 0) {
+            f.run(source -> ((SourceAccessor) source).calculateReverb(g, this.listener));
+            /*world
+	        .getRegistryManager()
+	        .get(Registry.BLOCK_KEY)
+	        .streamTags()
+	        .forEachOrdered(tagKey -> Engine.LOGGER.info(tagKey.registry().getValue()));*/
+        }
+    }
 }
