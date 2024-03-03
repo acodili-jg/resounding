24826bc14b5e2538b3bdf36c6242ee985ce7c383
Jack Gabriel Acodili <112671361+acodili-jg@users.noreply.github.com>
2024-03-03 15:40:07 +0800

    Format SourceMixin.java

--- src/main/java/dev/thedocruby/resounding/mixin/SourceMixin.java
+++ src/main/java/dev/thedocruby/resounding/mixin/SourceMixin.java
@@ -15,31 +15,35 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
 @Environment(EnvType.CLIENT)
 @Mixin(Source.class)
 public class SourceMixin implements SourceAccessor {
+    @Shadow
+    @Final
+    private int pointer;
 
-	@Shadow
-	@Final
-	private int pointer;
+    private Vec3d soundPos;
 
-	private Vec3d soundPos;
+    @Inject(method = "setPosition", at = @At("HEAD"))
+    private void savePosition(final Vec3d pos, final CallbackInfo callback) {
+        if (Engine.on) {
+            this.soundPos = pos;
+        }
+    }
 
-	@Inject(method = "setPosition", at = @At("HEAD"))
-	private void savePosition(Vec3d pos, CallbackInfo ci) { if (!Engine.on) return; this.soundPos = pos; }
+    @Inject(method = "play", at = @At("HEAD"))
+    private void onPlay(final CallbackInfo callback) {
+        if (Engine.on) {
+            // TODO make context dynamic
+            Engine.play(Engine.root, soundPos, pointer, false);
+        }
+    }
 
-	@Inject(method = "play", at = @At("HEAD"))
-	private void onPlay(CallbackInfo ci) {
-		if (!Engine.on) return;
-		// TODO make context dynamic
-		Engine.play(Engine.root, soundPos, pointer, false);
-	}
-
-	public void calculateReverb(SoundInstance sound, SoundListener listener) {
-		if (!Engine.on) return;
-		Engine.recordLastSound(sound, listener);
-		// TODO make context dynamic
-		Engine.play(Engine.root, soundPos, pointer, false);
-	}
+    public void calculateReverb(final SoundInstance sound, final SoundListener listener) {
+        if (Engine.on) {
+            Engine.recordLastSound(sound, listener);
+            // TODO make context dynamic
+            Engine.play(Engine.root, soundPos, pointer, false);
+        }
+    }
 }
